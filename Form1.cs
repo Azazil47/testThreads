1f307048fad6541aec4fd7e8f9a3325e64a88c4e
Azazil47 <72367218+Azazil47@users.noreply.github.com>
2020-10-07 23:17:00 +0700

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadTest2
+{
+    public partial class Form1 : Form
+    {
+        public void getText() {
+            for (int i = 0; i < 10; i++)
+            {
+                textBox1.Text += "1";
+                Thread.Sleep(2000);
+            }
+            
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            getText();
+        }
+    }
+}
