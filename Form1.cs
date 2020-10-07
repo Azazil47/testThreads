33a9ba2986189b77102753828a7851544104fe57
Azazil47 <72367218+Azazil47@users.noreply.github.com>
2020-10-07 23:27:33 +0700

    Сомнительны комит

--- Form1.cs
+++ Form1.cs
@@ -12,14 +12,21 @@ namespace ThreadTest2
 {
     public partial class Form1 : Form
     {
-        public void getText() {
+        public delegate void MyDelegate();
+        public void getText()
+        {
             for (int i = 0; i < 10; i++)
             {
                 textBox1.Text += "1 ";
                 Thread.Sleep(2000);
             }
-            
         }
+
+        public void funk()
+        {
+            BeginInvoke(new MyDelegate(getText));
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -27,7 +34,8 @@ namespace ThreadTest2
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
-            Thread thread = new Thread(getText);
+            Thread thread = new Thread(funk);
+            thread.Start();
             
         }
     }
